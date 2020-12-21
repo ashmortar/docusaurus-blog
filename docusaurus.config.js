1f41902d01dab1229121c8798eea4160562eb9d5
Aaron Ross <aaron.ross@eyecuelab.com>
2020-12-20 18:16:54 -0800

    fix link url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/midi.svg',
   organizationName: 'ashmortar', // Usually your GitHub org/user name.
-  projectName: 'my-site', // Usually your repo name.
+  projectName: 'docusaurus-blog', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'ashmortar.io',
@@ -18,7 +18,7 @@ module.exports = {
       items: [
         {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/ashmotar',
+          href: 'https://github.com/ashmortar',
           label: 'GitHub',
           position: 'right',
         },
@@ -75,9 +75,6 @@ module.exports = {
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
